95737e15eb2d9fb247ad9619482e0e8763a1e419
anovinc <a2novinc@gmail.com>
2023-02-21 14:33:42 +0100

    Set up project

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -19,22 +19,8 @@ class MainActivity : ComponentActivity() {
       NoteAppTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-          Greeting("Android")
         }
       }
     }
-  }
-}
-
-@Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-  NoteAppTheme {
-    Greeting("Android")
   }
 }
