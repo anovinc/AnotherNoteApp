1969445624f36140086c8060b68ebd4714891611
anovinc <a2novinc@gmail.com>
2023-02-22 09:57:39 +0100

    Wrap use cases for single feature/viewModel

---
+++ app/src/main/java/com/example/noteapp/featureNote/domain/useCase/NoteUseCases.kt
@@ -0,0 +1,7 @@
+package com.example.noteapp.featureNote.domain.useCase
+
+//Wrap use cases for single feature/viewmodel in case viewmodel constructor became massive
+data class NoteUseCases(
+  val getNotesUseCase: GetNotesUseCase,
+  val deleteNoteUseCase: DeleteNoteUseCase
+)
